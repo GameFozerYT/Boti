7e82228efbcb241421fb0e365ad39d3088cf1c54
GameFozerYT <48291236+GameFozerYT@users.noreply.github.com>
2019-04-07 23:06:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -489,14 +489,4 @@ message.channel.send('**وعليكم السلام**');
    }
    });
 
-
-  client.on("message", message => {
-    if (message.content === "من افضل صديق عندك يا بوت") {
-   
-message.channel.send('**mochamadpro**');
-
-   }
-   });
-
-
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
